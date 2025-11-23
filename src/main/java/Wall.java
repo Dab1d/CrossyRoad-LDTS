3b7dc2bc94aca04e486e42cc2e0a742c064e12bd
David <2006davidferreira@gmail.com>
2025-11-23 23:12:22 +0000

    Add Wall class

---
+++ src/main/java/Wall.java
@@ -0,0 +1,18 @@
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+import java.io.IOException;
+
+public class Wall extends Element {
+    //constructor
+    public Wall(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void draw(TextGraphics graphics) throws IOException {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#808080"));
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "#");
+    }
+}
